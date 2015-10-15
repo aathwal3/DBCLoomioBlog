3f5861e3683acb5c17c84c3604f1d4e12d16b8d8
aathwal3 <aathwal3@gmail.com>
2015-10-14 19:45:08 -0700

    add about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+  	@posts = Post.all
   end
+
+  def show
+  end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   get 'home/index'
-  
-  resources :home
+  get 'home/about'
+
+  resources :home 
+
+  # scope about do 
+  #   get 'about' => 'home#about'
+  # end
+
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
