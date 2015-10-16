e218a9cd4653afc4d522437df22d706a768fbd74
aathwal3 <aathwal3@gmail.com>
2015-10-15 17:45:24 -0700

    SUCCESSFUL change to postql database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151014220458) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "title"
